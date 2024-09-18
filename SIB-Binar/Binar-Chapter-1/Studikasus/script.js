420e8bbdde843af80ff150f37afb94a36dfeddc0
Rafijoee <rafijoe44@gmail.com>
2024-09-18 20:54:12 +0700

    feat (study case)

--- SIB-Binar/Binar-Chapter-1/Studikasus/script.js
+++ SIB-Binar/Binar-Chapter-1/Studikasus/script.js
@@ -18,17 +18,37 @@ console.log (`Jumlah huruf vokal pada kalimat "${kalimat}" adalah ${vocal(kalima
 
 // Polindrom
 
-function polindrom(str){
-    let huruf = str.toLowerCase().replace(/[^a-z]/g, '');
-    let reverse = huruf.split('').reverse().join('');
+// function polindrom(str){
+//     let huruf = str.toLowerCase().replace(/[^a-z]/g, '');
+//     let reverse = huruf.split('').reverse().join('');
 
-    return huruf === reverse;
-}
+//     return huruf === reverse;
+// }
 
-let kata = prompt('Masukkan kata: ');   
-if (polindrom(kata)){
-    console.log(`${kata} adalah polindrom`);
-} else {
-    console.log(`${kata} bukan polindrom`);
-}
+// let kata = prompt('Masukkan kata: ');   
+// if (polindrom(kata)){
+//     console.log(`${kata} adalah polindrom`);
+// } else {
+//     console.log(`${kata} bukan polindrom`);
+// }
 
+//method class hewan
+
+// class Hewan {
+
+//     constructor (nama, jenis){
+//         this.nama = nama;
+//         this.jenis = jenis;
+//     }
+
+//     bersuara (suara){
+//         console.log(`${this.nama} bersuara ${suara}`);
+//     }
+// }
+
+// let hewan1 = new Hewan('Kucing', 'Mamalia');
+// console.log(hewan1);
+// hewan1.bersuara('Meong');
+// let hewan2 = new Hewan('anjing', 'Macan Gunung')
+// console.log(hewan2);
+// hewan2.bersuara('Guk Guk');
