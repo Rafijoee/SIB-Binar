ec9c8376aeb8dcbaa352cefbf8d1216d4c9f9110
Rafijoee <rafijoe44@gmail.com>
2024-09-18 20:17:02 +0700

    feat (studi kasus polindrom)

--- SIB-Binar/Binar-Chapter-1/Studikasus/script.js
+++ SIB-Binar/Binar-Chapter-1/Studikasus/script.js
@@ -1,3 +1,5 @@
+//Huruf vocal !
+/*
 function vocal (str){
     let hurufVokal = 'aiueo';
     let jumlahVokal = 0;
@@ -12,4 +14,21 @@ function vocal (str){
 
 let kalimat  = prompt('Masukkan kalimat: ');
 console.log (`Jumlah huruf vokal pada kalimat "${kalimat}" adalah ${vocal(kalimat)}`);
+*/
+
+// Polindrom
+
+function polindrom(str){
+    let huruf = str.toLowerCase().replace(/[^a-z]/g, '');
+    let reverse = huruf.split('').reverse().join('');
+
+    return huruf === reverse;
+}
+
+let kata = prompt('Masukkan kata: ');   
+if (polindrom(kata)){
+    console.log(`${kata} adalah polindrom`);
+} else {
+    console.log(`${kata} bukan polindrom`);
+}
 
