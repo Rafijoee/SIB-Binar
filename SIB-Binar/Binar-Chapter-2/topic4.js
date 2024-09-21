459f95801fc4d0291ae3269bb6a38ba101814ef9
Rafijoee <rafijoe44@gmail.com>
2024-09-21 10:49:02 +0700

    feat (topic4.interhitance)

--- SIB-Binar/Binar-Chapter-2/topic4.js
+++ SIB-Binar/Binar-Chapter-2/topic4.js
@@ -1,14 +1,46 @@
-const strArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+// //interhitance
 
-function foreach (array, callback){
-    const newArray = [];
-    for (let i = 0; i < array.length; i++){
-        newArray.push(callback(array[i], i));
-    }
-    return newArray;
-}
+// class Human {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+//   introduce() {
+//     console.log(`Hello, my name is ${this.name}, I am ${this.age} years old`);
+//   }
+//   work() {
+//     console.log('I am working');
+//   }
+// }
 
-const lenArray = foreach(strArray, (item) => {
-    return item.length;
-});
-console.log(lenArray); // [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+// class Programmers extends Human {
+//   constructor(name, age, language) {
+//     super(name, age);
+//     this.language = language;
+//   }
+//   introduce() {
+//     super.introduce();
+//     console.log(`I am a programmer, I can code in ${this.language}`);
+//   }
+//   code() {
+//     console.log('I am coding', this.language[Math.floor(Math.random() * this.language.length)]);
+//   }
+// }
+
+// let obama = new Human('Obama', 55);
+// obama.introduce();
+
+// let bill = new Programmers('Bill', 22, ['JavaScript', 'Python', 'Java']);
+// bill.introduce();
+// bill.code();
+// bill.work();
+
+// try {
+//   obama.code();
+// } catch (error) {
+//     console.log('Obama is not a programmer');
+//     console.log(error.message);
+//     }
+
+// console.log(bill instanceof Human);
+// console.log(bill instanceof Programmers);
