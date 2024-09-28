a41950241128a08167552a3c39a9fe87d55a400c
Rafijoee <rafijoe44@gmail.com>
2024-09-28 23:04:14 +0700

    feat : soal baru di oopstucase3

--- SIB-Binar/Binar-Chapter-2/oopstucase3.js
+++ SIB-Binar/Binar-Chapter-2/oopstucase3.js
@@ -37,93 +37,155 @@
 // - create two methods of calculating the area and the price per area. If the color is blue the price will be twice the area and if the color is grey, the price will be three times the area. Otherwise, the price will be the same as the area.
 // - Create instances of the 'Triangle' and 'Cicle' class and display its prices for red, blue, purple, and grey colors
 
-class Shape {
-    constructor(name) {
-        if (this.constructor === Shape) {
-            throw new Error("Kelas Shape adalah abstrak dan tidak bisa diinstansiasi langsung.");
-        }
-        this.name = name;
-    }   
-}
+// class Shape {
+//     constructor(name) {
+//         if (this.constructor === Shape) {
+//             throw new Error("Kelas Shape adalah abstrak dan tidak bisa diinstansiasi langsung.");
+//         }
+//         this.name = name;
+//     }   
+// }
 
-class Circle extends Shape {
-    #color; // properti private untuk color
+// class Circle extends Shape {
+//     #color; // properti private untuk color
 
-    constructor(radius, color) {
-        super("Circle");
-        this.radius = radius;
-        this.#color = color;
+//     constructor(radius, color) {
+//         super("Circle");
+//         this.radius = radius;
+//         this.#color = color;
+//     }
+
+//     // Menghitung luas lingkaran
+//     calculateArea() {
+//         return Math.PI * this.radius * this.radius;
+//     }
+
+//     // Menghitung harga berdasarkan warna
+//     calculatePrice() {
+//         const area = this.calculateArea();
+//         switch (this.#color.toLowerCase()) {
+//             case 'blue':
+//                 return area * 2;
+//             case 'grey':
+//                 return area * 3;
+//             default:
+//                 return area;
+//         }
+//     }
+
+//     // Metode untuk menampilkan harga
+//     displayPrice() {
+//         console.log(`Harga untuk ${this.name} dengan warna ${this.#color} adalah $${this.calculatePrice().toFixed(2)}`);
+//     }
+// }
+
+// class Triangle extends Shape {
+//     #color; // properti private untuk color
+//     constructor(side, color) {
+//         super('Circle');
+//         this.side = side;
+//         this.#color = color;
+//     }
+
+//     calculateArea() {
+//         return Math.pow(this.side, 2) * Math.sqrt(3) / 4;
+//     }
+
+//     calculatePricePerArea() {
+//         const area = this.calculateArea();
+//         if (this.#color === "blue") {
+//             return area * 2;
+//         } else if (this.#color === "grey") {
+//             return area * 3;
+//         } else {
+//             return area;
+//         }
+//     }
+// }
+
+
+
+// const redCircle = new Circle(5, 'red');
+// const blueCircle = new Circle(5, 'blue');
+// const purpleCircle = new Circle(5, 'purple');
+// const greyCircle = new Circle(5, 'grey');
+
+// redCircle.calculatePrice();
+// redCircle.displayPrice();
+// blueCircle.calculatePrice();
+// blueCircle.displayPrice();
+// purpleCircle.calculatePrice();
+// purpleCircle.displayPrice();
+// greyCircle.calculatePrice();
+// greyCircle.displayPrice();
+
+// const redTriangle = new Triangle(5, 'red');
+// const blueTriangle = new Triangle(5, 'blue');
+// const purpleTriangle = new Triangle(5, 'purple');
+// const greyTriangle = new Triangle(5, 'grey');
+
+// console.log(redTriangle.calculatePricePerArea());
+// console.log(blueTriangle.calculatePricePerArea());
+// console.log(purpleTriangle.calculatePricePerArea());
+
+
+// Studi Kasus: Sistem Manajemen Bank
+// Bayangkan kita membuat aplikasi untuk mengelola berbagai jenis akun bank, seperti akun tabungan (savings account) dan akun giro (current account). Setiap akun memiliki fitur dasar seperti setoran (deposit), penarikan (withdraw), dan pengecekan saldo (check balance). Namun, masing-masing akun memiliki aturan spesifik, seperti bunga pada tabungan dan limit overdraft pada akun giro.
+
+// 1. Encapsulation: Menyembunyikan detail internal akun seperti saldo.
+// 2. Inheritance: Akun tabungan dan giro akan mewarisi sifat umum dari akun bank.
+// 3. Polymorphism: Operasi seperti withdraw akan memiliki perilaku yang berbeda tergantung jenis akun.
+
+class BankAccount {
+    constructor (id, balance) {
+        this.id = id;
+        this._balance = balance;
     }
 
-    // Menghitung luas lingkaran
-    calculateArea() {
-        return Math.PI * this.radius * this.radius;
-    }
-
-    // Menghitung harga berdasarkan warna
-    calculatePrice() {
-        const area = this.calculateArea();
-        switch (this.#color.toLowerCase()) {
-            case 'blue':
-                return area * 2;
-            case 'grey':
-                return area * 3;
-            default:
-                return area;
-        }
-    }
-
-    // Metode untuk menampilkan harga
-    displayPrice() {
-        console.log(`Harga untuk ${this.name} dengan warna ${this.#color} adalah $${this.calculatePrice().toFixed(2)}`);
-    }
-}
-
-class Triangle extends Shape {
-    #color; // properti private untuk color
-    constructor(side, color) {
-        super('Circle');
-        this.side = side;
-        this.#color = color;
-    }
-
-    calculateArea() {
-        return Math.pow(this.side, 2) * Math.sqrt(3) / 4;
-    }
-
-    calculatePricePerArea() {
-        const area = this.calculateArea();
-        if (this.#color === "blue") {
-            return area * 2;
-        } else if (this.#color === "grey") {
-            return area * 3;
+    deposit(amount) {
+        if (amount > 0) {
+            this._balance += amount;
+            return `Deposit $${amount} berhasil. Saldo sekarang $${this._balance}`;
         } else {
-            return area;
+            return 'Deposit gagal. Masukkan jumlah yang valid.';
         }
     }
 }
 
+class SavingsAccount extends BankAccount {
+    constructor(id, balance, interestRate) {
+        super(id, balance);
+        this.interestRate = interestRate;
+    }
 
+    addInterest() {
+        const interest = this._balance * (this.interestRate / 100);
+        this._balance += interest;
+        return `Bunga $${interest} berhasil ditambahkan. Saldo sekarang $${this._balance}`;
+    }
 
-const redCircle = new Circle(5, 'red');
-const blueCircle = new Circle(5, 'blue');
-const purpleCircle = new Circle(5, 'purple');
-const greyCircle = new Circle(5, 'grey');
+    withdraw(amount) {
+        if (amount > 0 && amount <= this._balance) {
+            this._balance -= amount;
+            return `Penarikan $${amount} berhasil. Saldo sekarang $${this._balance}`;
+        } else {
+            return 'Penarikan gagal. Masukkan jumlah yang valid.';
+        }
+    }
+}
 
-redCircle.calculatePrice();
-redCircle.displayPrice();
-blueCircle.calculatePrice();
-blueCircle.displayPrice();
-purpleCircle.calculatePrice();
-purpleCircle.displayPrice();
-greyCircle.calculatePrice();
-greyCircle.displayPrice();
+class Giro extends BankAccount {
+    constructor(id, balance, overdraftLimit) {
+        super(id, balance);
+        this.overdraftLimit = overdraftLimit;
+    }
 
-const redTriangle = new Triangle(5, 'red');
-const blueTriangle = new Triangle(5, 'blue');
-const purpleTriangle = new Triangle(5, 'purple');
-const greyTriangle = new Triangle(5, 'grey');
-
-console.log(redTriangle.calculatePricePerArea());
-console.log(blueTriangle.calculatePricePerArea());
-console.log(purpleTriangle.calculatePricePerArea());
+    withdraw(amount) {
+        if (amount > 0 && amount <= this._balance + this.overdraftLimit) {
+            this._balance -= amount;
+            return `Penarikan $${amount} berhasil. Saldo sekarang $${this._balance}`;
+        } else {
+            return 'Penarikan gagal. Melebihi limit overdraft.';
+        }
+    }
+}
