44154cb304ad92a2cc0215b29f6317e07caca2ce
Rafijoee <rafijoe44@gmail.com>
2024-10-11 21:56:19 +0700

    feat : Chapter 4 Topic 2

--- SIB-Binar/Binar-Chapter-4/create.js
+++ SIB-Binar/Binar-Chapter-4/create.js
@@ -1,12 +1,12 @@
-const createPerson = function (person){
-    fs.writeFileSync('person.json', JSON.stringify(person));
-    return person;
-}
+// const createPerson = function (person){
+//     fs.writeFileSync('person.json', JSON.stringify(person));
+//     return person;
+// }
 
-const sabrina = createPerson({
-    name: 'Sabrina',
-    age: 22,
-    address: 'Jakarta'
-});
+// const sabrina = createPerson({
+//     name: 'Sabrina',
+//     age: 22,
+//     address: 'Jakarta'
+// });
 
 
--- SIB-Binar/Binar-Chapter-4/index.js
+++ SIB-Binar/Binar-Chapter-4/index.js
@@ -1,13 +1,17 @@
-const os = require('os')
-const fs = require('fs')
+const os = require("os");
+const fs = require("fs");
 
-const sabrina = require ('./create.js')
-const luasSegitiga = require('./segitiga.js')
+// const luasSegitiga = require("./segitiga.js");
 
-console.log(sabrina)
+// console.log("free memory :", os.freemem());
+// console.log("hello world !");
 
-console.log('free memory :', os.freemem())
-console.log('hello world !')
+// console.log(luasSegitiga(10, 5)); // 25
 
-console.log(luasSegitiga(10, 5)) // 25
+const express = require("express");
+const app = express();
+const port = 3000;
 
+app.get("/", (req, res) => res.send("Hello World!, ini adalah halaman utama"));
+
+app.listen(port, () => console.log(`Example app listening at port http://localhost:${port}!`));
