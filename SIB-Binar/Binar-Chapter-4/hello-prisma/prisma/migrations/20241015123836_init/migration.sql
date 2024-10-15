b19fae9726a1aa452fa45a570f461136bb91bc5e
Rafijoee <rafijoe44@gmail.com>
2024-10-15 19:43:34 +0700

    feat : Migrate User and Post with Prisma

---
+++ SIB-Binar/Binar-Chapter-4/hello-prisma/prisma/migrations/20241015123836_init/migration.sql
@@ -0,0 +1,49 @@
+/*
+  Warnings:
+
+  - The primary key for the `Post` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `content` on the `Post` table. All the data in the column will be lost.
+  - You are about to drop the column `createdAt` on the `Post` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `Post` table. All the data in the column will be lost.
+  - The `id` column on the `Post` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `createdAt` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `email` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `User` table. All the data in the column will be lost.
+  - The `id` column on the `User` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+  - A unique constraint covering the columns `[name]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Changed the type of `authorId` on the `Post` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_authorId_fkey";
+
+-- DropIndex
+DROP INDEX "User_email_key";
+
+-- AlterTable
+ALTER TABLE "Post" DROP CONSTRAINT "Post_pkey",
+DROP COLUMN "content",
+DROP COLUMN "createdAt",
+DROP COLUMN "updatedAt",
+DROP COLUMN "id",
+ADD COLUMN     "id" SERIAL NOT NULL,
+ALTER COLUMN "published" SET DEFAULT true,
+DROP COLUMN "authorId",
+ADD COLUMN     "authorId" INTEGER NOT NULL,
+ADD CONSTRAINT "Post_pkey" PRIMARY KEY ("id");
+
+-- AlterTable
+ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
+DROP COLUMN "createdAt",
+DROP COLUMN "email",
+DROP COLUMN "updatedAt",
+DROP COLUMN "id",
+ADD COLUMN     "id" SERIAL NOT NULL,
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_name_key" ON "User"("name");
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
