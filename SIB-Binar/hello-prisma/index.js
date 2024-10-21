89ad877761cfc6c06f00fafd7a531e4cd676309b
Rafijoee <rafijoe44@gmail.com>
2024-10-21 20:19:57 +0700

    feat : install swagger

--- SIB-Binar/hello-prisma/index.js
+++ SIB-Binar/hello-prisma/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {PrismaClient}  = require('@prisma/client');
-
+const joi = require('joi');
 
 const app = express();
 const port = 3000;
 const prisma = new PrismaClient();
 
+const swaggerJSON = require(/../swagger.json);
+const swaggerUI = require('swagger-ui-express');
+
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerJSON));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -26,7 +30,7 @@ app.post('/api/v1/users', async (req, res) => {
 
         res.status(201).json(user);
     } catch (error) {
-        res.status(500).json({ error: error.message });
+        res.status(500).json({ error: error.message});
     }
 });
 
@@ -54,6 +58,58 @@ app.post('/api/v1/categories', async (req, res) => {
                             create: {
                                 name: 'New Category'
                             },
+                        }
+                    }
+                ]
+            }
+        }
+    });
+
+    res.status(201).json(createCategory);
+});
+
+app.post('/api/v1/posts', async (req, res) => {
+    const createCategory = await prisma.post.create({
+        data: {
+            title: 'How to be Bob',
+            categories: {
+                create: [
+                    {
+                        assignedBy: 'Bob',
+                        assignedAt: new Date(),
+                        category: {
+                            connect: {
+                                id: 1
+                            },
+                        }
+                    }
+                ]
+            }
+        }
+    });
+
+    res.status(201).json(createCategory);
+});
+
+app.post('/api/v1/posts2', async (req, res) => {
+    const createCategory = await prisma.post.create({
+        data: {
+            title: 'How to be Catalina',
+            categories: {
+                create: [
+                    {
+                        assignedBy: 'Catalina',
+                        assignedAt: new Date(),
+                        category: {
+                            connectOrCreate: {
+                                where: {
+                                    id: 1
+                                },
+                                create: {
+                                    name: 'New Category',
+                                    id: 9
+                                },
+                            }
                         }
                     }
                 ]
